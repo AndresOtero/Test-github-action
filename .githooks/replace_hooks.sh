9fdd370ab14e1d95655e77f5bea5187f097d82df
andy <oteroandres95@gmail.com>
2020-10-13 14:14:52 -0300

    Test version again

--- .githooks/replace_hooks.sh
+++ .githooks/replace_hooks.sh
@@ -1,5 +1,5 @@
 #! /bin/bach
 root="$(dirname $(dirname "$0"))"
 echo $root
-ln -sf "$root/.githooks"/* "$root/.git/hooks"
+cp "$root/.githooks"/* "$root/.git/hooks"
 echo "$root/.githooks/* replaces $root/.git/hooks" 
